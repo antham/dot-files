f649d4d31bd7b848340b6958a42c477527bf7af9
antham <hamonanth@gmail.com>
2011-05-18 21:00:01 +0200

    Add init file

---
+++ init.sh
@@ -0,0 +1,7 @@
+ln -fs ~/dot-files/stumpwmrc ~/.stumpwmrc
+ln -fs ~/dot-files/xinitrc ~/.xinitrc
+ln -fs ~/dot-files/xmodmaprc ~/.xmodmaprc
+ln -fs ~/dot-files/Xresources ~/.Xresources
+ln -fs ~/dot-files/tmux.conf ~/.tmux.conf
+ln -fs ~/dot-files/zsh/ ~/.zsh
+ln -fs ~/dot-files/zshrc ~/.zshrc
