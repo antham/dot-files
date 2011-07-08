99bf770f5e9e18308528f08660b910ce354f0ab5
antham <hamonanth@gmail.com>
2011-07-09 01:14:36 +0200

    Add config file for acoc soft

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ ln -fs ~/dot-files/xinitrc ~/.xinitrc
 ln -fs ~/dot-files/xmodmaprc ~/.xmodmaprc
 ln -fs ~/dot-files/Xresources ~/.Xresources
 ln -fs ~/dot-files/tmux.conf ~/.tmux.conf
+ln -fs ~/dot-files/acoc.conf ~/.acoc.conf
 ln -fs ~/dot-files/zsh/ ~/.zsh
 ln -fs ~/dot-files/zshrc ~/.zshrc
