735655895bc39c54f8fc7e4a8888bb2b541902b0
antham <hamonanth@gmail.com>
2013-09-15 11:29:47 +0200

    [Gitconfig] Add gitconfig

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ ln -fs ~/dot-files/Xresources ~/.Xresources
 ln -fs ~/dot-files/tmux.conf ~/.tmux.conf
 ln -fs ~/dot-files/acoc.conf ~/.acoc.conf
 ln -fs ~/dot-files/conkerorrc ~/.conkerorrc
+ln -fs ~/dot-files/gitconfig ~/.gitconfig
 ln -fs ~/dot-files/zsh/ ~/.zsh
 ln -fs ~/dot-files/zshrc ~/.zshrc
