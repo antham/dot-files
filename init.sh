3e8ca5d279eb3579db415cba02402745ae7b00a3
antham <hamonanth@gmail.com>
2016-01-14 21:44:34 +0100

    Add fish

--- init.sh
+++ init.sh
@@ -9,5 +9,6 @@ ln -fs ~/dot-files/gitconfig ~/.gitconfig
 ln -fs ~/dot-files/zsh/ ~/.zsh
 ln -fs ~/dot-files/zshrc ~/.zshrc
 ln -fs ~/dot-files/cwmrc ~/.cwmrc
+ln -fs ~/dot-files/config.fish ~/.config/fish/config.fish
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
