f4ff7cd855a2d2074745253722dcf642a14cdbf8
Anthony HAMON <hamon.anth@gmail.com>
2021-05-20 22:02:01 +0200

    Clean config, remove useless stuffs

--- init.sh
+++ init.sh
@@ -1,13 +1,8 @@
-ln -fs ~/dot-files/stumpwmrc ~/.stumpwmrc
 ln -fs ~/dot-files/xinitrc ~/.xinitrc
 ln -fs ~/dot-files/xmodmaprc ~/.xmodmaprc
 ln -fs ~/dot-files/Xresources ~/.Xresources
 ln -fs ~/dot-files/tmux.conf ~/.tmux.conf
-ln -fs ~/dot-files/acoc.conf ~/.acoc.conf
-ln -fs ~/dot-files/conkerorrc ~/.conkerorrc
 ln -fs ~/dot-files/gitconfig ~/.gitconfig
-ln -fs ~/dot-files/zsh/ ~/.zsh
-ln -fs ~/dot-files/zshrc ~/.zshrc
 ln -fs ~/dot-files/cwmrc ~/.cwmrc
 ln -fs ~/dot-files/config.fish ~/.config/fish/config.fish
 
