5fcca569c8e4fe85d7a558a1e70237578849ffba
Anthony HAMON <hamon.anth@gmail.com>
2021-06-01 20:58:41 +0200

    Add starship config

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ln -fs ~/dot-files/xinitrc ~/.xinitrc
 ln -fs ~/dot-files/xmodmaprc ~/.xmodmaprc
 ln -fs ~/dot-files/Xresources ~/.Xresources
@@ -5,5 +7,6 @@ ln -fs ~/dot-files/tmux.conf ~/.tmux.conf
 ln -fs ~/dot-files/gitconfig ~/.gitconfig
 ln -fs ~/dot-files/cwmrc ~/.cwmrc
 ln -fs ~/dot-files/config.fish ~/.config/fish/config.fish
+ln -fs ~/dot-files/starship.toml ~/.config/starship.toml
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
