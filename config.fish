159947932dc881a4c3d2e671a48099cbf5465356
Anthony HAMON <hamon.anth@gmail.com>
2021-06-05 22:46:36 +0200

    Setup VISUAL and EDITOR environment variable

--- config.fish
+++ config.fish
@@ -2,9 +2,13 @@ alias cat=bat
 alias ls=exa
 alias e="emacsclient -t"
 alias j=z
+
 set fzf_preview_file_cmd cat
 set fzf_preview_dir_cmd exa --all --color=always
 set fzf_fd_opts --hidden --exclude=.git
+set EDITOR "emacsclient -t"
+set VISUAL "emacsclient -t"
+
 zoxide init fish | source
 starship init fish | source
 
